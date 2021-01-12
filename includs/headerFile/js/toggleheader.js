c05242f6ee4726f09aad0d497e1614fd1218c104
yusuf <yossefazzam2@gmail.com>
2021-01-12 22:09:41 +0200

    add game page and few changes in project

--- includs/headerFile/js/toggleheader.js
+++ includs/headerFile/js/toggleheader.js
@@ -8,3 +8,11 @@ toggleButton.onclick = function() {
 toggle_time.onclick = function() {
     boxToggle.style.right = "-90%";
 }
+
+// button-warnning
+var button_warnning = document.querySelector(".button-warnning"),
+    box_warnning = document.querySelector(".check-login");
+
+button_warnning.onclick = function() {
+    box_warnning.style.display = "none";
+}
